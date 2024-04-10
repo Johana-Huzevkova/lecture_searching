70c5be743e7ad543d926a489f76ff47e5f5dd773
Johana-Huzevkova <johca03@gmail.com>
2024-04-10 10:09:31 +0200

    Add function for searching pattern

--- searching.py
+++ searching.py
@@ -33,11 +33,24 @@ def linear_search(unordered_numbers, searched_number):
     return dict
 
 
+def pattern_search(dna_sequence, searched_pattern):
+    pattern_index = set()
+    for idx in range(len(dna_sequence)):
+        sequence = dna_sequence[idx:idx + len(searched_pattern)]
+        if sequence == searched_pattern:
+            pattern_index.add(int(idx + ((len(searched_pattern)) / 2)))
+
+    return pattern_index
+
+
 def main():
     unordered_numbers = read_data("sequential.json", "unordered_numbers")
     searched_number = 0
     search = linear_search(unordered_numbers, searched_number)
-    print(search)
+    dna_sequence = read_data("sequential.json", "dna_sequence")
+    searched_pattern = "ATA"
+    pattern = pattern_search(dna_sequence, searched_pattern)
+    print(pattern)
 
 
 if __name__ == '__main__':
